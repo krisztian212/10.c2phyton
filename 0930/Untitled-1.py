f5be8a61912593e701bbb4f28bd270ea54c4e32e
Sándor Krisztián <sandor.krisztian@sulinet.csmszki>
2021-10-07 09:38:25 +0200

    elagazas

---
+++ 0930/Untitled-1.py
@@ -0,0 +1,47 @@
+# kerekítés
+print(round(3.1415,3))
+print(round(3,1425.2,))
+# hatványozás
+print(pow(2, 8))#pow alap kitevő
+#abszolútérték abs(mit)
+print(abs(-12))
+print(abs(12))
+print(min(12,4,3))
+print(max(12,4,3))
+# pi
+'''
+import math 
+a=math.pi
+print(a)
+'''
+from math import* #program elején helyezzük el
+a=pi
+print(a)
+#négyzetgyök -sqrt
+print(sqrt(16)) #math.sqrt (miből vonok gyököt)
+# felfelé kerekítés - ceil(Mit kerekítek)
+print(ceil(3.56))
+# lefelé kerekítés math.floor (mit)
+print(floor(3.56))
+
+# 1. a négyzet kerülete területe
+a=float(input ("kérem a négyzet területét,kerületét! a="))
+k=4*a
+t=pow(a,2)
+
+
+
+
+# 2. téglalap kerülete területe
+input(kerület(a+b*2)
+input(terület(a*b*2))
+
+
+
+# 3. kör területe kerülete (2^r*pi)
+
+
+# 4. kocka térfogota (V=a*3) felszíne (a=6*a^2)
+
+# 5. derékszögű háromszög 2 befogóját kérd be add meg at átfogót!
+# c=négyzetgyök(a^2+b^2)
